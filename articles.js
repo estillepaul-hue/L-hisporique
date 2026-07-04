50e86aa57d2d70fde70b993b0c96e5026b8de595
estillepaul-hue <estille.paul@gmail.com>
2026-07-04 11:13:47 +0200

    Update articles.js

--- articles.js
+++ articles.js
@@ -6,10 +6,7 @@
 // et change les informations entre guillemets " ".
 //
 // section : "football"  ou  "autres-sports"  ou  "actu"
-// featured : mets "true" sur UN SEUL article, celui que tu veux
-//            voir en gros en haut de la page d'accueil.
-// content : le texte complet de l'article. Pour faire un
-//           paragraphe, sépare le texte par \n\n (comme ci-dessous).
+
 
 const articles = [
   {
