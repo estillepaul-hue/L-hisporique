cf56446ff6c6bb8e4bf91785bf68498039907f14
estillepaul-hue <estille.paul@gmail.com>
2026-07-04 11:15:15 +0200

    Update articles.js

--- articles.js
+++ articles.js
@@ -1,11 +1,6 @@
 // ============================================================
 // ARTICLES DE "L'HISPORIQUE"
 // ============================================================
-// Pour ajouter un article : copie un bloc { ... } ci-dessous,
-// colle-le juste après l'accolade [ qui suit cette ligne,
-// et change les informations entre guillemets " ".
-//
-// section : "football"  ou  "autres-sports"  ou  "actu"
 
 
 const articles = [
